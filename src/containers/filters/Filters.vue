84d87b39a6550efb01f0813418776804af9f5c7d
ShreyasVH <shreyas.deve.1@gmail.com>
2025-11-17 18:45:10 +0530

    fab for filters

---
+++ src/containers/filters/Filters.vue
@@ -0,0 +1,13 @@
+<template>
+  <v-btn color="primary" icon="mdi-filter-variant" class="ma-2" style="position: fixed; bottom: 16px; right: 16px;" />
+</template>
+
+<script>
+export default {
+  name: "Filters"
+}
+</script>
+
+<style scoped>
+
+</style>
---
+++ src/containers/players/Stats.vue
@@ -0,0 +1,18 @@
+<template>
+Stats
+    <Filters />
+</template>
+
+<script>
+import Filters from '../filters/Filters.vue';
+export default {
+  name: "Stats",
+  components: {
+    Filters
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
