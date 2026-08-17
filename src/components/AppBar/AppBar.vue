772f62e38adf62c40d0dbdb37da3190aaf36e213
ShreyasVH <44293779+ShreyasVH@users.noreply.github.com>
2026-08-17 12:46:19 +0530

    support drawer
    * intermediate commit
    
    * hiding dropdown
    
    * clearing up
    
    * supporting dark theme
    
    * supporting drawer
    
    * added icons

--- src/components/AppBar/AppBar.vue
+++ src/components/AppBar/AppBar.vue
@@ -1,7 +1,15 @@
 <template>
   <v-app-bar color="primary" absolute>
     <template v-slot:prepend>
-      <v-app-bar-nav-icon />
+      <v-app-bar-nav-icon @click="toggleDrawer"></v-app-bar-nav-icon>
+    </template>
+
+    <template v-slot:append>
+      <SearchSelect :on-select="handlePlayerSelect" />
+
+      &nbsp;&nbsp;
+
+      <ThemeSelector />
     </template>
 
     <template v-slot:append>
@@ -14,14 +22,21 @@
   </v-app-bar>
 
   <v-navigation-drawer
+      v-model="drawerOpen"
       temporary
   >
+    <v-list nav>
+      <v-list-item prepend-icon="mdi-home" title="Home" value="item1" @click="() => navigateToLink('/')"></v-list-item>
+
+      <v-list-item prepend-icon="mdi-chart-bar" title="Players Stats" value="item2" @click="() => navigateToLink('/players/stats')"></v-list-item>
+    </v-list>
   </v-navigation-drawer>
 </template>
 
 <script>
 import SearchSelect from '../SearchSelect/SearchSelect.vue';
 import ThemeSelector from '../ThemeSelector.vue';
+import { useRouter } from 'vue-router';
 
 export default {
   name: "AppBar",
@@ -29,9 +44,31 @@ export default {
     SearchSelect,
     ThemeSelector
   },
+  setup() {
+    const router = useRouter()
+
+    const navigateToLink = (path) => {
+      router.push({
+        path
+      })
+    }
+
+    return {
+      navigateToLink
+    }
+  },
+  data () {
+    return {
+      drawerOpen: false
+    }
+  },
   methods: {
     handlePlayerSelect: function (event, item) {
       this.$router.push(`/players/details?id=${item.id}`);
+    },
+
+    toggleDrawer () {
+      this.drawerOpen = !this.drawerOpen;
     }
   }
 }
